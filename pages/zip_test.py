935f20e7cbdd665292857c5171eedff6fe78cbf3
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-24 14:53:14 -0500

    -

--- pages/zip_test.py
+++ pages/zip_test.py
@@ -4,7 +4,7 @@ from zipfile import ZipFile
 import base64
 
 #Show files in specified directory
-dir_list = os.listdir("/mount/src/spanish-app/Sentences/Audio/Week 9/")
+dir_list = os.listdir("/mount/src/spanish-app/Sentences/")
 st.write(dir_list)
 
 #os.remove('/mount/src/spanish-app/Sentences/Audio/audio_files.zip')
