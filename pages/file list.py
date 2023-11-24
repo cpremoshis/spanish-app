a809c9bf60ad360dd42c78aeadf6cd48e6aa3e97
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-24 15:02:56 -0500

    -

--- pages/file list.py
+++ pages/file list.py
@@ -7,6 +7,26 @@ import base64
 dir_list = os.listdir("/mount/src/spanish-app/")
 st.write(dir_list)
 
+def print_directory_contents(path):
+    for root, dirs, files in os.walk(path):
+        print(f"Current Directory: {root}")
 
+        # Print subdirectories
+        print("Subdirectories:")
+        for directory in dirs:
+            print(os.path.join(root, directory))
+
+        # Print files
+        print("Files:")
+        for file in files:
+            print(os.path.join(root, file))
+
+        print("")
+
+sentences_path = "/mount/src/spanish-app/Sentences"
+audio_path = "/mount/src/spanish-app/Audio"
+
+st.write(print_directory_contents(sentences_path))
+st.write(print_directory_contents(vocab_path))
 
 #os.remove('/mount/src/spanish-app/Sentences/Audio/audio_files.zip')
