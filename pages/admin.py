b8c6b0e5e5a00a55b17d4a94496ae730a25dc8d3
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-24 14:44:02 -0500

    -

--- pages/admin.py
+++ pages/admin.py
@@ -69,33 +69,32 @@ with tab1:
                     )
 
             with download_audio_col:
-                if st.button("Download audio"):
 
-                    def zip_folder(folder_path, zip_filename):
-                        if not os.path.exists(folder_path):
-                            print(f"Error: Folder '{folder_path}' not found.")
-                            return
-                        
-                        with ZipFile(zip_filename, 'w') as zip_file:
-                            for foldername, subfolders, filenames in os.walk(folder_path):
-                                for filename in filenames:
-                                    file_path = os.path.join(foldername, filename)
-                                    zip_file.write(file_path, os.path.relpath(file_path, folder_path))
+                def zip_folder(folder_path, zip_filename):
+                    if not os.path.exists(folder_path):
+                        print(f"Error: Folder '{folder_path}' not found.")
+                        return
+                    
+                    with ZipFile(zip_filename, 'w') as zip_file:
+                        for foldername, subfolders, filenames in os.walk(folder_path):
+                            for filename in filenames:
+                                file_path = os.path.join(foldername, filename)
+                                zip_file.write(file_path, os.path.relpath(file_path, folder_path))
 
-                        print(f"Zip file '{zip_filename}' created successfully.")
+                    print(f"Zip file '{zip_filename}' created successfully.")
 
-                    folder_path = "/mount/src/spanish-app/Sentences/Audio/" + week_selected + "/"
-                    zip_filename = "/mount/src/spanish-app/Sentences/Audio/audio_files.zip"
+                folder_path = "/mount/src/spanish-app/Sentences/Audio/" + week_selected + "/"
+                zip_filename = "/mount/src/spanish-app/Sentences/Audio/audio_files.zip"
 
-                    if st.button("Generate zip file"):
-                        zip_folder(folder_path, zip_filename)
+                if st.button("Generate zip file"):
+                    zip_folder(folder_path, zip_filename)
 
-                    if os.path.exists(zip_filename):
-                        with open(zip_filename, 'rb') as f:
-                            zip_contents = f.read()
-                            b64 = base64.b64encode(zip_contents).decode()
-                            href = f'<a href="data:file/zip;base64,{b64}" download="{zip_filename}">Download zip</a>'
-                            st.markdown(href, unsafe_allow_html=True)
+                if os.path.exists(zip_filename):
+                    with open(zip_filename, 'rb') as f:
+                        zip_contents = f.read()
+                        b64 = base64.b64encode(zip_contents).decode()
+                        href = f'<a href="data:file/zip;base64,{b64}" download="{zip_filename}">Download zip</a>'
+                        st.markdown(href, unsafe_allow_html=True)
         
 with tab2:
     feedback_file = "/mount/src/spanish-app/Feedback/reports.txt"
