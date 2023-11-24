de1029221711a7a5ada2f0cdf77e175471cfb772
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-24 15:06:24 -0500

    -

--- pages/file list.py
+++ pages/file list.py
@@ -7,26 +7,11 @@ import base64
 dir_list = os.listdir("/mount/src/spanish-app/")
 st.write(dir_list)
 
-def print_directory_contents(path):
-    for root, dirs, files in os.walk(path):
-        print(f"Current Directory: {root}")
+sentences_start_path = '/mount/src/spanish-app/Sentences/'
 
-        # Print subdirectories
-        print("Subdirectories:")
-        for directory in dirs:
-            print(os.path.join(root, directory))
-
-        # Print files
-        print("Files:")
-        for file in files:
-            print(os.path.join(root, file))
-
-        print("")
-
-sentences_path = "/mount/src/spanish-app/Sentences/"
-audio_path = "/mount/src/spanish-app/Audio/"
-
-st.write(print_directory_contents(sentences_path))
-st.write(print_directory_contents(audio_path))
+for root, dirs, files in os.walk(sentences_start_path):
+	level = root.replace(sentences_start_path, '').count(os.sep)
+	indent = ' ' * 4 * (level)
+	st.write('{}{}/'.format(indent, os.path.basename(root)))
 
 #os.remove('/mount/src/spanish-app/Sentences/Audio/audio_files.zip')
