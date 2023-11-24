12f272f168ce049854b30c399af02e2307f1acf5
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-23 19:53:48 -0500

    -

--- pages/admin.py
+++ pages/admin.py
@@ -88,7 +88,7 @@ with tab1:
 
                         buttons.empty()
 
-                        with open(zipped_file, 'r') as f2:
+                        with open(zipped_file, 'rb') as f2:
                             download_button = st.download_button(
                                 label = "Download audio",
                                 data = f2,
