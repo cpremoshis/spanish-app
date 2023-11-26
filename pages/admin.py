317459b7a9bd954ab27579997a2cf134f4d8e455
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-25 20:21:11 -0500

    -

--- pages/admin.py
+++ pages/admin.py
@@ -95,126 +95,23 @@ def create_sentences_from_vocab(vocab_df):
 
 
 #API key
-try:
-    pass
+#try:
     #openai.api_key = st.secrets['openai']['api_key']
     #config = configparser.ConfigParser()
     #config.read('/Users/casey/Documents/PythonProjects/Spanish Learning App/config.ini')
     #openai.api_key = config['openai']['api_key']
-except:
-    st.error("Access denied.")
+#except:
+#    pass
 
 
 try:
     tab1, tab2, tab3, tab4 = st.tabs(['Upload/Download Files', 'Generate content', 'Feedback', 'File list'])
 
     with tab1:
-        with st.form("data_selection_form"):
-            data_column1, data_column2, data_column3, data_column4 = st.columns(4)
-
-            with data_column1:
-                action_selection = st.selectbox("Action:", ['Upload', 'Download'])
-            with data_column2:
-                type_selection = st.selectbox("File type:", ['Vocab', 'Sentences'])
-            with data_column3:
-                week_selection = st.text_input("Week:")
-                week_selection = "Week " + str(week_selection)
-            with data_column4:
-                st.markdown(" ")
-                st.text("")
-                submit_button = st.form_submit_button("Confirm selection")
-
-        st.write("Selection: " + type_selection + " - " + action_selection + " - " + week_selection)
-
-        #Needed inputs for processing of uploaded CSV
-        df_columns = ['Spanish', 'English']
-        sentence_csv_path = "/mount/src/spanish-app/Sentences/" + week_selection + "/"
-        sentence_audio_path = sentence_csv_path + "audio/"
-
-        if action_selection == 'Upload':
-            uploaded_file = st.file_uploader("Choose a file. Processing begins immediately upon upload.")
-            if uploaded_file is not None:
-                file_df = pd.read_csv(uploaded_file, names=df_columns)
-
-                st.write("Preview:")
-                st.write(file_df.head())
-
-                processing_container = st.empty()
-                processing_container.warning('Processing file. Please wait.')
-                
-                status = create_sentences_from_vocab(file_df)
-                
-                if status == 1:
-                    processing_container.success("Processing success.")
-                else:
-                    processing_container.write(status)
-
-        if action_selection == "Download":
-            
-            if type_selection == "Vocab":
-                vocab_files_path = "/mount/src/spanish-app/Vocab/"
-                vocab_files_list = os.listdir(vocab_files_path)
-                vocab_files_list.remove(".DS_Store")
-                vocab_file_selected = st.selectbox("Select file:", vocab_files_list)
-                vocab_file_to_download = vocab_files_path + vocab_file_selected
-
-                with open(vocab_file_to_download, 'r') as f:
-                    download_button = st.download_button(
-                        label = "Download file",
-                        data = f,
-                        file_name = vocab_file_selected,
-                        mime = 'text/csv'
-                        )
-                    
-            if type_selection == "Sentences":
-                sentences_csv_path = "/mount/src/spanish-app/Sentences/" + week_selection + "/"
-                sentences_csv_list = os.listdir(sentences_csv_path)
-                sentences_csv_list.remove(".DS_Store")
-                sentences_csv_list.remove("audio")
-                week_selection = st.selectbox("Select file:", sentences_csv_list)
-                sentence_csv_to_download = sentences_csv_path + week_selection
-
-                download_csv_col, download_audio_col = st.columns(2)
-
-                with download_csv_col:
-                    with open(sentence_csv_to_download, 'r') as f:
-                        download_button = st.download_button(
-                            label = "Download CSV",
-                            data = f,
-                            file_name = week_selection,
-                            mime = 'text/csv'
-                        )
-
-                with download_audio_col:
-
-                    def zip_folder(folder_path, zip_filename):
-                        if not os.path.exists(folder_path):
-                            print(f"Error: Folder '{folder_path}' not found.")
-                            return
-                        
-                        with ZipFile(zip_filename, 'w') as zip_file:
-                            for foldername, subfolders, filenames in os.walk(folder_path):
-                                for filename in filenames:
-                                    file_path = os.path.join(foldername, filename)
-                                    zip_file.write(file_path, os.path.relpath(file_path, folder_path))
-
-                        print(f"Zip file '{zip_filename}' created successfully.")
-
-                    folder_path = "/mount/src/spanish-app/Sentences/" + week_selection + "/audio/"
-                    zip_filename = "/mount/src/spanish-app/Sentences/" + week_selection + "/" + "audio_files.zip"
-
-                    if st.button("Generate zip file"):
-                        zip_folder(folder_path, zip_filename)
-
-                    if os.path.exists(zip_filename):
-                        with open(zip_filename, 'rb') as f:
-                            zip_contents = f.read()
-                            b64 = base64.b64encode(zip_contents).decode()
-                            href = f'<a href="data:file/zip;base64,{b64}" download="{week_selection + ".zip"}">Download zip</a>'
-                            st.markdown(href, unsafe_allow_html=True)
+        st.error("Access denied.")
             
     with tab2:
-        st.error("Under construction.")
+        st.error("Access denied.")
 
     with tab3:
         
@@ -236,7 +133,7 @@ try:
             st.error("No file found.")
 
     with tab4:
-        st.error("Under construction.")
+        st.error("Access denied.")
 except:
     st.error("Access denied.")
 
