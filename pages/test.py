f1df29af6838e54e1eef48e3bebc7ac28a457af5
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-28 00:24:56 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -5,9 +5,9 @@ import pandas as pd
 file_path = "cpremoshis/spanish-app/Sentences/Economia/Economía.csv"
 
 
-    try:
-        with open(file_path, 'r', encoding='utf-8') as f:
-            df = pd.read_csv(f, index_col=0)
-        st.write(df)
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+try:
+    with open(file_path, 'r', encoding='utf-8') as f:
+        df = pd.read_csv(f, index_col=0)
+    st.write(df)
+except Exception as e:
+    st.error(f"An error occurred: {e}")
