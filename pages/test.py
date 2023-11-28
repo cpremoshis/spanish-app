e73096c1882aae6428455ab01e016f9f34df9231
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-28 13:55:27 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -5,7 +5,7 @@ import unicodedata
 
 
 def open_sentences():
-    sentences = "/Users/casey/Documents/PythonProjects/Spanish Learning App/Sentences/Economía/Economía.csv"
+    sentences = "./Sentences/Economía/Economía.csv"
 
     with open(sentences, 'r') as f:
         sentences_df = pd.read_csv(f, index_col=0)
