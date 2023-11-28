91b245d3dbc77085329854051aec082c16a89eaf
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-28 00:24:40 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -4,12 +4,10 @@ import pandas as pd
 
 file_path = "cpremoshis/spanish-app/Sentences/Economia/Economía.csv"
 
-if os.path.isfile(file_path):
+
     try:
         with open(file_path, 'r', encoding='utf-8') as f:
             df = pd.read_csv(f, index_col=0)
         st.write(df)
     except Exception as e:
         st.error(f"An error occurred: {e}")
-else:
-    st.error(f"File not found: {file_path}")
