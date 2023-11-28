a774d1dd67d2547453089487df4fa020ef0d49cd
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-28 12:00:58 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -12,14 +12,6 @@ def open_topics_list():
     with open(chat_topics_file, 'r') as f:
         topics_df = pd.read_csv(f, names=col_names)
 
-    def normalize(row):
-        pre_normalized = row['Week']
-        normalized = unicodedata.normalize('NFC', pre_normalized)
-
-        return normalized
-    
-    topics_df[:,'Week'] = topics_df.apply(normalize, axis=1)
-
     return topics_df
 
 topics_df = open_topics_list()
