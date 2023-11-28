2ac5e27d3f0042c5b16984d4f4b6a61b04761461
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-28 17:08:35 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -25,5 +25,5 @@ with col2:
 
 #st.write(senteces_df[senteces_df['Week'] == 'USAID']['Topics'])
 
-files = os.listdir("/mount/admin/")
+files = os.listdir("/mount/admin/streamlit-version/")
 st.write(files)
