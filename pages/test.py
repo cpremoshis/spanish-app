7ad0c236a1a32d64dd168729f15a80cebd85a2b0
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-28 13:46:56 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -3,34 +3,23 @@ import os
 import pandas as pd
 import unicodedata
 
-def find_sentence_files():
-    sentences_root_folder = "./Sentences"
-    sentence_dir_list = os.listdir(sentences_root_folder)
-    sentence_dir_list.remove(".DS_Store")
-    sentence_files = {}
 
-    for item in sentence_dir_list:
-        folder_contents = os.listdir(sentences_root_folder + "/" + item)
+def open_topics_list():
 
-        for file in folder_contents:
-            if ".csv" in file:
-                final_file = file
-                sentence_files[file.strip('.csv')] = sentences_root_folder + "/" + item + "/" + final_file
+    chat_topics_file = "./Chat Topics/Chat Topics.csv"
+    col_names = ['Week', 'Topics']
 
-    return sentence_files
+    with open(chat_topics_file, 'r') as f:
+        topics_df = pd.read_csv(f, names=col_names, encoding='utf-8')
 
-sentence_files = find_sentence_files()
-
-st.write(sentence_files)
-
-def open_sentences(week):
- 
-    with open(sentence_files[week], 'r') as f:
-        sentences_df = pd.read_csv(f, index_col=0, encoding='utf-8')
+    def normalize_characters(text):
+        return unicodedata.normalize('NFC', text)
     
-    return sentences_df
+    topics_df['Week'] = topics_df['Week'].apply(normalize_characters)
 
+    return topics_df
 
+topics_df = open_topics_list()
 
 #st.write(topics_df)
 
