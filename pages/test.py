252964654f0399389c6f6b70fcbd2a57a1ced89a
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-28 10:06:06 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -11,7 +11,19 @@ try:
 except Exception as e:
     st.error(f"An error occurred: {e}")
 
-st.audio('./Sentences/Política/audio/1_Política_audio.mp3')
+import unicodedata
+
+# Fetch the path from DataFrame
+audio_path_df = df.iloc[0]['Audio']
+
+# Normalize and re-encode
+audio_path_df_normalized = unicodedata.normalize('NFKD', audio_path_df).encode('ascii', 'ignore').decode('ascii')
+
+# Try using the normalized path
+st.audio(audio_path_df_normalized)
+
+
+#st.audio('./Sentences/Política/audio/1_Política_audio.mp3')
 
 
 #st.audio(audio_path)
