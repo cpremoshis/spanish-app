4b04c4f356765961e01c013b8e8331e9b4756f0b
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-28 00:19:21 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -2,9 +2,14 @@ import streamlit as st
 import os
 import pandas as pd
 
-file = "cpremoshis/spanish-app/Sentences/Economía/Economía.csv"
+file_path = "cpremoshis/spanish-app/Sentences/Economía/Economía.csv"
 
-with open('cpremoshis/spanish-app/Sentences/Economía/Economía.csv', 'r') as f:
-    df = pd.read_csv(f, index_col=0, encoding='utf-8')
-
-st.write(df)
+if os.path.isfile(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            df = pd.read_csv(f, index_col=0)
+        st.write(df)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+else:
+    st.error(f"File not found: {file_path}")
