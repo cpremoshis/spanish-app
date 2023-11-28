997bb051fa6b53827d110aed3439d25a009efd54
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-28 00:07:56 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -5,6 +5,6 @@ import pandas as pd
 file = "cpremoshis/spanish-app/Sentences/Economía/Economía.csv"
 
 with open('cpremoshis/spanish-app/Sentences/Economía/Economía.csv', 'r') as f:
-    df = pd.read_csv(f, index_col=0)
+    df = pd.read_csv(f, index_col=0, encoding='utf-8')
 
 st.write(df)
