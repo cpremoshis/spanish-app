8ccc5fcc65a784e344569d3378a746edd8a4b19a
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-30 00:20:28 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -13,18 +13,8 @@ def open_topics_list():
 
 topics_list, files_df = open_topics_list()
 
-def open_sentences(topic):
-    sentences_df = None
-
-    sentence_path = files_df[files_df['Topics'] == topic]['Sentences'].iloc[0]
-    sentence_path = unicodedata.normalize('NFC', sentence_path)
-
-    with open(sentence_path, 'r') as f:
-        sentences_df = pd.read_csv(f, index_col=0, encoding='utf-8')
-
-    return sentences_df
-
-sentences_df = open_sentences('Economía')
+sentence_path = files_df[files_df['Topics'] == topic]['Sentences'].iloc[0]
+sentence_path = unicodedata.normalize('NFC', sentence_path)
 
 hardcode = '/mount/src/spanish-app/Sentences/Economía/Economía.csv'
 df_path = files_df[files_df['Topics'] == 'Sentences']['Sentences'].iloc[0]
@@ -33,4 +23,4 @@ col1, col2 = st.columns(2)
 with col1:
     st.write("Hardcoded path bytes:", [hex(ord(c)) for c in hardcode])
 with col2:
-    st.write("DataFrame path bytes:", [hex(ord(c)) for c in df_path])
+    st.write("DataFrame path bytes:", [hex(ord(c)) for c in sentence_path])
