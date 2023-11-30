8ec8426526b894e7c75b7254478d8522ee95efbe
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-30 11:59:41 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -11,16 +11,20 @@ def open_topics_list():
 
     return topics_list, files_df
 
-topics_list, files_df = open_topics_list()
+#topics_list, files_df = open_topics_list()
 
-sentence_path = files_df[files_df['Topics'] == 'Economía']['Sentences'].iloc[0]
-sentence_path = unicodedata.normalize('NFC', sentence_path)
+#sentence_path = files_df[files_df['Topics'] == 'Economía']['Sentences'].iloc[0]
+#sentence_path = unicodedata.normalize('NFC', sentence_path)
 
-hardcode = '/mount/src/spanish-app/Sentences/Economía/Economía.csv'
-df_path = files_df[files_df['Topics'] == 'Sentences']['Sentences'].iloc[0]
+#hardcode = '/mount/src/spanish-app/Sentences/Economía/Economía.csv'
+#df_path = files_df[files_df['Topics'] == 'Sentences']['Sentences'].iloc[0]
 
-col1, col2 = st.columns(2)
-with col1:
-    st.write("Hardcoded path bytes:", [hex(ord(c)) for c in hardcode])
-with col2:
-    st.write("DataFrame path bytes:", [hex(ord(c)) for c in sentence_path])
+#col1, col2 = st.columns(2)
+#with col1:
+#    st.write("Hardcoded path bytes:", [hex(ord(c)) for c in hardcode])
+#with col2:
+#    st.write("DataFrame path bytes:", [hex(ord(c)) for c in sentence_path])
+
+
+test = os.path.isfile('/mount/src/spanish-app/Topics.csv')
+st.write(test)
