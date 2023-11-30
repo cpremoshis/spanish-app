0999ac22c1ab073a4bdbd886e993d349fa133d1c
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-11-29 22:35:06 -0500

    switched to DataFrame usage

--- app.py
+++ app.py
@@ -356,25 +356,6 @@ def find_vocab_files():
 
 vocab_files = find_vocab_files()
 
-@st.cache_data()
-def find_sentence_files():
-    sentences_root_folder = "./Sentences"
-    sentence_dir_list = os.listdir(sentences_root_folder)
-    sentence_dir_list.remove(".DS_Store")
-    sentence_files = {}
-
-    for item in sentence_dir_list:
-        folder_contents = os.listdir(sentences_root_folder + "/" + item)
-
-        for file in folder_contents:
-            if ".csv" in file:
-                final_file = file
-                sentence_files[file.strip('.csv')] = sentences_root_folder + "/" + item + "/" + final_file
-
-    return sentence_files
-
-sentence_files = find_sentence_files()
-
 #Sets default 'word_index'
 if 'word_index' not in st.session_state:
     st.session_state.word_index = 0
