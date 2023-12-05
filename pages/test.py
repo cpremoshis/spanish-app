e0b6a01a104f12a6e498d2c44d5beef707cae5cd
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-12-05 09:41:36 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -13,25 +13,25 @@ def open_topics_list():
 
 topics_list, files_df = open_topics_list()
 
-#sentence_path = files_df[files_df['Topics'] == 'Economía']['Sentences'].iloc[0]
-#sentence_path = unicodedata.normalize('NFC', sentence_path)
-
-#hardcode = '/mount/src/spanish-app/Sentences/Economía/Economía.csv'
-#df_path = files_df[files_df['Topics'] == 'Sentences']['Sentences'].iloc[0]
-
-#col1, col2 = st.columns(2)
-#with col1:
-#    st.write("Hardcoded path bytes:", [hex(ord(c)) for c in hardcode])
-#with col2:
-#    st.write("DataFrame path bytes:", [hex(ord(c)) for c in sentence_path])
-
-
-
-
-sentence_path = '/mount/src/spanish-app/Sentences/Economía/Economía.csv'
+sentence_path = files_df[files_df['Topics'] == 'Economía']['Sentences'].iloc[0]
 sentence_path = unicodedata.normalize('NFC', sentence_path)
 
-with open(sentence_path, 'r') as f:
-    test_df = pd.read_csv(f)
+hardcode = '/mount/src/spanish-app/Sentences/Economía/Economía.csv'
+df_path = files_df[files_df['Topics'] == 'Sentences']['Sentences'].iloc[0]
 
-st.write(test_df)
+col1, col2 = st.columns(2)
+with col1:
+    st.write("Hardcoded path bytes:", [hex(ord(c)) for c in hardcode])
+with col2:
+    st.write("DataFrame path bytes:", [hex(ord(c)) for c in sentence_path])
+
+
+
+
+#sentence_path = '/mount/src/spanish-app/Sentences/Economía/Economía.csv'
+#sentence_path = unicodedata.normalize('NFC', sentence_path)
+
+#with open(sentence_path, 'r') as f:
+#    test_df = pd.read_csv(f)
+
+#st.write(test_df)
