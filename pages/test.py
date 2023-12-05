223f759396b5998aaa6a496ac50f5bdf45934ded
cpremoshis <106867548+cpremoshis@users.noreply.github.com>
2023-12-05 09:44:03 -0500

    -

--- pages/test.py
+++ pages/test.py
@@ -13,17 +13,17 @@ def open_topics_list():
 
 topics_list, files_df = open_topics_list()
 
-sentence_path = files_df[files_df['Topics'] == 'Economia']['Sentences'].iloc[0]
-sentence_path = unicodedata.normalize('NFC', sentence_path)
+#sentence_path = files_df[files_df['Topics'] == 'Economia']['Sentences'].iloc[0]
+#sentence_path = unicodedata.normalize('NFC', sentence_path)
 
 hardcode = '/mount/src/spanish-app/Sentences/Economia/Economia.csv'
-df_path = files_df[files_df['Topics'] == 'Sentences']['Sentences'].iloc[0]
+#df_path = files_df[files_df['Topics'] == 'Sentences']['Sentences'].iloc[0]
 
 col1, col2 = st.columns(2)
 with col1:
     st.write("Hardcoded path bytes:", [hex(ord(c)) for c in hardcode])
-with col2:
-    st.write("DataFrame path bytes:", [hex(ord(c)) for c in sentence_path])
+#with col2:
+#    st.write("DataFrame path bytes:", [hex(ord(c)) for c in sentence_path])
 
 #sentence_path = '/mount/src/spanish-app/Sentences/Economía/Economía.csv'
 #sentence_path = unicodedata.normalize('NFC', sentence_path)
